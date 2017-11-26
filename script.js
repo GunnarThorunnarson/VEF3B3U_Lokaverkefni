e1482bcc7ffc806833c037c8d483ad92984f99d1
MikaelMani99 <mikaeelmani99@gmail.com>
2017-11-26 16:32:00 +0000

    prufa rotationið

--- script.js
+++ script.js
@@ -1,28 +1,23 @@
-alert("Halloe");
 window.addEventListener("orientationchange", function() {
     alert("the orientation of the device is now " + screen.orientation.angle);
+
+    if (window.matchMedia("(orientation: portrait)").matches) {
+       // you're in PORTRAIT mode
+       console.log("portrait");
+    }
+    if (window.matchMedia("(orientation: landscape)").matches) {
+       // you're in LANDSCAPE mode
+       alert("prufa")
+       //Event listener sem að lætur í fullscreen þegar ýtt er á skjáinn
+       addEventListener("click", function() {
+        let el = document.documentElement;
+        let rfs = el.requestFullscreen
+            || el.webkitRequestFullScreen
+            || el.mozRequestFullScreen
+            || el.msRequestFullscreen;
+
+        rfs.call(el);
+
+      });
+    }
 });
-/*
-window.addEventListener('orientationchange', main);
-function main() {
-  if (window.matchMedia("(orientation: portrait)").matches) {
-     // you're in PORTRAIT mode
-     console.log("portrait");
-  }
-  if (window.matchMedia("(orientation: landscape)").matches) {
-     // you're in LANDSCAPE mode
-     alert("prufa")
-     //Event listener sem að lætur í fullscreen þegar ýtt er á skjáinn
-     addEventListener("click", function() {
-      let el = document.documentElement;
-      let rfs = el.requestFullscreen
-          || el.webkitRequestFullScreen
-          || el.mozRequestFullScreen
-          || el.msRequestFullscreen;
-
-      rfs.call(el);
-
-    });
-  }
-}
-*/
