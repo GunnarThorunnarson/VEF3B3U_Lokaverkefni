5d46f7db7ec187bd10610d42d11edabc7cf98807
MikaelMani99 <mikaeelmani99@gmail.com>
2017-11-26 16:18:31 +0000

    prufa

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ if (window.matchMedia("(orientation: portrait)").matches) {
 }
 if (window.matchMedia("(orientation: landscape)").matches) {
    // you're in LANDSCAPE mode
+   alert("prufa")
    //Event listener sem að lætur í fullscreen þegar ýtt er á skjáinn
    addEventListener("click", function() {
     let el = document.documentElement;
