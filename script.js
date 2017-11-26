0d10b2eb3b590e766bd3d0aa3d35bf57d20bb14a
MikaelMani99 <mikaeelmani99@gmail.com>
2017-11-26 16:23:18 +0000

    detecta orientation change

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
-if (window.matchMedia("(orientation: portrait)").matches) {
-   // you're in PORTRAIT mode
-   console.log("portrait");
-}
-if (window.matchMedia("(orientation: landscape)").matches) {
-   // you're in LANDSCAPE mode
-   alert("prufa")
-   //Event listener sem að lætur í fullscreen þegar ýtt er á skjáinn
-   addEventListener("click", function() {
-    let el = document.documentElement;
-    let rfs = el.requestFullscreen
-        || el.webkitRequestFullScreen
-        || el.mozRequestFullScreen
-        || el.msRequestFullscreen;
 
-    rfs.call(el);
+window.addEventListener('orientationchange', main);
+function main() {
+  if (window.matchMedia("(orientation: portrait)").matches) {
+     // you're in PORTRAIT mode
+     console.log("portrait");
+  }
+  if (window.matchMedia("(orientation: landscape)").matches) {
+     // you're in LANDSCAPE mode
+     alert("prufa")
+     //Event listener sem að lætur í fullscreen þegar ýtt er á skjáinn
+     addEventListener("click", function() {
+      let el = document.documentElement;
+      let rfs = el.requestFullscreen
+          || el.webkitRequestFullScreen
+          || el.mozRequestFullScreen
+          || el.msRequestFullscreen;
 
-  });
+      rfs.call(el);
+
+    });
+  }
 }
