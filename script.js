74f749d9469ef5ad306fb5361c7cb9ed1d6be635
MikaelMani99 <mikaeelmani99@gmail.com>
2017-11-29 08:32:34 +0000

    downloadað src/ fyrir leikinn

--- script.js
+++ script.js
@@ -6,34 +6,52 @@
          console.log("portrait");
       }
       else if (window.matchMedia("(orientation: landscape)").matches) {
-         // you're in LANDSCAPE mode
-         let canvas = document.getElementById("canvas");
-         //widthin og heigtið á canvasinum
-         canvas.width  = window.innerWidth;
-         canvas.height = window.innerHeight;
-         //náð í context úr canvasinum
-         let ctx = canvas.getContext('2d');
-         let maxWidth = Math.round(canvas.width);
-         let maxHeigt = Math.round(canvas.height);
-         //reiknað út stærðina á grísnum
-         let size = Math.round(maxWidth * 0.07);
-         // fundið upphafstaðsetninguna á grísnum
-         let yPos = maxHeigt - size;
-         let xPos = Math.round(maxWidth/2) - size/2;
-         //þetta er stærðin á grísnum
-         let pig = new Image();
-         pig.src = "myndir/gris.png";
-         //teiknað grísinn
-         ctx.drawImage(pig, xPos, yPos, size, (size * 0.8));
 
 
-         function clearCanvas(){
-           ctx.clearRect(0, 0, canvas.width, canvas.height);
-         }
 
-        console.log("hallo");
       }
   });
+
+  function setUpGame(){
+    let canvas = document.getElementById("canvas");
+    //widthin og heigtið á canvasinum
+    canvas.width  = window.innerWidth;
+    canvas.height = window.innerHeight;
+    //náð í context úr canvasinum
+    let ctx = canvas.getContext('2d');
+    let maxWidth = Math.round(canvas.width);
+    let maxHeigt = Math.round(canvas.height);
+    //reiknað út stærðina á grísnum
+    let size = Math.round(maxWidth * 0.07);
+    // fundið upphafstaðsetninguna á grísnum
+    let yPos = maxHeigt - size;
+    let xPos = Math.round(maxWidth/2) - size/2;
+    //þetta er stærðin á grísnum
+    let pig = new Image();
+    pig.src = "myndir/gris.png";
+    //teiknað grísinn
+    ctx.drawImage(pig, xPos, yPos, size, (size * 0.8));
+  }
+
+  function clearCanvas(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  }
+  function check(){
+    if (!window.screenTop && !window.screenY) {
+      console.log('fullscreen');
+    } else {
+      console.log('not fullscreen');
+    }
+  }
+  addEventListener('webkitfullscreenchange', function(e) {
+    check();
+  }, false);
+  addEventListener('mozfullscreenchange', function(e) {
+    check();
+  }, false);
+  addEventListener('fullscreenchange', function(e) {
+    check();
+  }, false);
   //Event listener sem að lætur í fullscreen þegar ýtt er á skjáinn
   addEventListener("click", function() {
    let el = document.documentElement;
