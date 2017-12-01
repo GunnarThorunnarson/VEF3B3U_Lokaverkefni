44416ac758cbec4144468aaa1da06a879b032a05
MikaelMani99 <mikaeelmani99@gmail.com>
2017-12-01 13:35:18 +0000

    virkar að hreyfa grís með skynjurum

--- script.js
+++ script.js
@@ -50,13 +50,13 @@ let ctx = canvas.getContext('2d');
   let move = setInterval(function(){
     clearCanvas();
     if (x > 0) {
-      let speed = 500/x;
+      let speed = 250/x;
       if ((xPos+Math.round(size) < canvas.width)) {
         xPos+= Math.round(size/speed);
       }
     }
     else if (x < 0) {
-      let speed = (500/x) * (-1);
+      let speed = (250/x) * (-1);
       if (xPos > 0) {
         xPos-= Math.round(size/speed);
       }
